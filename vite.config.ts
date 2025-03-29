407c78931d0d49d086389e4c337474eae21a04ba
Connor Abbas <abbasconnor@gmail.com>
2025-03-29 18:22:37 +0000

    Up and running

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const devPort = parseInt(env.VITE_PORT) || 5173;
+    const hostDomain = env.VITE_HOST_DOMAIN || 'localhost';
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            port: devPort,
+            host: true,
+            hmr: {
+                host: hostDomain,
+            },
+            cors: true,
+            watch: {
+                usePolling: true,
+            },
+        },
+        preview: {
+            port: devPort,
+        },
+    });
+};
